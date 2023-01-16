5891a3f55d49a99fe5eda5faa8d36dda28528e55
Slawomir Dyk <slawomir.dyk@astrosat.net>
2023-01-16 20:05:53 +0000

    add the main-page component

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,3 +1,33 @@
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useEffect, useState } from "react";
+import { Post } from "./post";
+
+export interface Post {
+  id: string;
+  userId: string;
+  username: string;
+  title: string;
+  description: string;
+}
 export const Main = () => {
-  return <h1>Main</h1>;
+  const postsRef = collection(db, "posts");
+  const [postsList, setPostList] = useState<Post[] | null>(null);
+  const getPosts = async () => {
+    const data = await getDocs(postsRef);
+    setPostList(
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
+    );
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+  getPosts();
+  return (
+    <h1>
+      {postsList?.map((post) => (
+        <Post post={post} />
+      ))}
+    </h1>
+  );
 };
